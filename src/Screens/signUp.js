7c11721b400513a066a81477dbeb7a5c6b5d7d45
Salman M <89529477+SalmanMoh-js@users.noreply.github.com>
2022-11-01 15:53:24 +0300

    Update signUp.js

--- src/Screens/signUp.js
+++ src/Screens/signUp.js
@@ -5,7 +5,6 @@ import {
   IconButton,
   Button as MaterialButton,
   Pressable,
-  TextInput,
   Divider,
 } from "@react-native-material/core";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
@@ -15,7 +14,7 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import Entypo from "@expo/vector-icons/Entypo";
 import Feather from "@expo/vector-icons/Feather";
 import tw from "twrnc";
-import { Button } from "react-native-paper";
+import { Button, TextInput } from "react-native-paper";
 import LoginSvg from "../../assets/loginSvg.svg";
 import { LinearGradient } from "expo-linear-gradient";
 import { ImageBackground } from "react-native";
@@ -64,20 +63,13 @@ const SignupScreen = () => {
         </View>
         <View className="w-full flex items-center justify-center p-4">
           <TextInput
+            theme={{ roundness: 30 }}
             placeholder="Name"
-            style={tw.style("w-11/12 my-2 rounded-lgs")}
-            value={signupData.name}
-            autoComplete="name-family"
-            onChangeText={(text) => {
-              setSignupData({
-                ...signupData,
-                name: text,
-              });
-            }}
-            leading={(props) => (
-              <AntDesign name="user" {...props} color="#4577a967" />
-            )}
-            variant="outlined"
+            style={tw.style("w-11/12 my-3 px-2 rounded-lg bg-white")}
+            left={
+              <TextInput.Icon icon="ios-person-outline" color="#4577a967" />
+            }
+            mode="outlined"
           />
           <PhoneInput
             ref={phoneInput}
@@ -97,57 +89,42 @@ const SignupScreen = () => {
               });
             }}
             containerStyle={tw.style(
-              "w-11/12 mb-3 rounded-md bg-white border border-gray-400"
+              "w-11/12 mb-2 rounded-full bg-white border border-gray-400"
             )}
-            textContainerStyle={tw.style("bg-white rounded-md")}
+            textContainerStyle={tw.style("bg-white rounded-full")}
             flagButtonStyle={tw.style("border-r border-gray-300")}
           />
           <TextInput
+            theme={{ roundness: 30 }}
             placeholder="Password"
             secureTextEntry={secureTextEntry}
-            value={signupData.password}
-            onChangeText={(text) => {
-              setSignupData({
-                ...signupData,
-                password: text,
-              });
-            }}
-            style={tw.style("w-11/12 mb-2")}
-            leading={(props) => (
-              <Feather name="lock" {...props} color="#4577a960" />
-            )}
-            trailing={(props) => (
-              <IconButton
-                icon={(props) => (
-                  <Ionicons
-                    name={
-                      secureTextEntry
-                        ? "ios-eye-off-outline"
-                        : "ios-eye-outline"
-                    }
-                    {...props}
-                    color="#4577a9"
-                  />
-                )}
-                {...props}
-                onPress={() => {
-                  if (secureTextEntry) {
-                    setSecureTextEntry(false);
-                  } else {
-                    setSecureTextEntry(true);
-                  }
-                }}
-                style={tw.style("z-50")}
-              />
-            )}
-            variant="outlined"
+            style={tw.style("w-11/12 mb-2 px-2 bg-white")}
+            left={
+              <TextInput.Icon icon="lock-closed-outline" color="#4577a960" />
+            }
+            right={
+              secureTextEntry ? (
+                <TextInput.Icon
+                  icon="eye-outline"
+                  color="#2a4563d2"
+                  onPress={() => setSecureTextEntry(false)}
+                />
+              ) : (
+                <TextInput.Icon
+                  icon="eye-off-outline"
+                  color="#2a4563d2"
+                  onPress={() => setSecureTextEntry(true)}
+                />
+              )
+            }
+            mode="outlined"
           />
           <Button
             mode="contained"
-            style={tw.style("w-4/5 mt-2 h-12 rounded-xl")}
+            style={tw.style("w-4/5 mt-2 h-12 rounded-full")}
+            labelStyle={tw.style("text-lg text-white")}
             contentStyle={tw.style("h-full")}
             color="#e09467"
-            labelStyle={tw.style("text-lg text-white")}
             onPress={() => navigation.navigate("Home")}
             uppercase={false}
           >
@@ -158,29 +135,35 @@ const SignupScreen = () => {
             <Text className="px-4 text-lg font-bold text-gray-600">Or</Text>
             <Divider style={tw.style("w-5/12")} />
           </View>
-          <Button
+          <MaterialButton
             mode="contained"
-            style={tw.style("w-4/5 mt-10 h-12")}
-            contentStyle={tw.style("h-full")}
-            icon="google"
+            style={tw.style("w-4/5 mt-10 p-0")}
+            contentContainerStyle={tw.style("p-2 w-full h-12")}
+            leading={(props) => (
+              <Icon
+                name="google"
+                {...props}
+                style={tw.style("text-blue-800")}
+              />
+            )}
             color="white"
-            labelStyle={tw.style("text-lg text-blue-800")}
+            titleStyle={tw.style("text-lg text-blue-800")}
             uppercase={false}
-          >
-            Sign up with Google
-          </Button>
-          <Button
+            title="Signup with Gmail"
+          />
+          <MaterialButton
             mode="contained"
             style={tw.style("w-4/5 mt-4 h-12")}
-            contentStyle={tw.style("h-full")}
-            icon="facebook"
+            contentContainerStyle={tw.style("p-2 w-full h-12")}
             color="#268ceb"
-            labelStyle={tw.style("text-lg text-white")}
+            titleStyle={tw.style("text-lg text-white")}
+            leading={(props) => (
+              <Icon name="facebook" {...props} color="white" />
+            )}
             uppercase={false}
-          >
-            Sign up with Facebook
-          </Button>
-          <View className="w-full flex flex-row mt-16 justify-center">
+            title="Signup with Facebook"
+          />
+          <View className="w-full flex flex-row mt-14 justify-center">
             <Text className="my-auto text-lg text-gray-700">
               Already have an account?
             </Text>
